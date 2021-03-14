d1caa05bce32e4abc8e30ef31686907ad2e20183
nahlaFathy <nehal.fathy.fci.scu@gmail.com>
2021-03-15 01:11:24 +0200

    ..

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/src/TODO-FE'));
+app.use(express.static(__dirname + '/src'));
   app.get('/*', function(req,res) {
     
     res.sendFile(path.join(__dirname+'/src/index.html'));
