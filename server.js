6484857f364975ae124004082fcb72aa83fe594d
nahlaFathy <nehal.fathy.fci.scu@gmail.com>
2021-03-15 00:55:45 +0200

    ..

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/todo-angulariti'));
+app.use(express.static(__dirname + '/TODO-FE'));
   app.get('/*', function(req,res) {
     
-    res.sendFile(path.join(__dirname+'/dist/todo-angulariti/index.html'));
+    res.sendFile(path.join(__dirname+'/TODO-FE/index.html'));
     });
 app.listen(process.env.PORT||3000);
