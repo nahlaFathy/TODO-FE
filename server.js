bd946969211e334e5d5b4f7b2974223f20d9ee94
nahlaFathy <nehal.fathy.fci.scu@gmail.com>
2021-03-15 02:09:14 +0200

    ..

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/Todo'));
   app.get('/*', function(req,res) {
     
-    res.sendFile(path.join(__dirname+'dist/Todo/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/Todo/index.html'));
     });
 app.listen(process.env.PORT||3000);
