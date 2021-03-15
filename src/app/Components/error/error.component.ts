19412861a7f06b7e11be4719e8238d749227cd89
nahlaFathy <nehal.fathy.fci.scu@gmail.com>
2021-03-15 03:45:08 +0200

    last edit

--- src/app/Components/error/error.component.ts
+++ src/app/Components/error/error.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
@@ -7,26 +7,10 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class ErrorComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) { }
-  closeResult
+  constructor() { }
+
   ngOnInit(): void {
   }
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
+  
 
 }
